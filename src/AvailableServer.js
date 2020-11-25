5bcf5ef270bc736188be334e02effac43fafd0ea
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2020-11-25 21:06:58 +0600

    Unexpected default export of anonymous function  import/no-anonymous-default-export fix

--- src/AvailableServer.js
+++ src/AvailableServer.js
@@ -1,33 +1,33 @@
 import React, {useState} from "react";
 
-export default function (props) {
+export default function AvailableServer(props) {
     const [filter, setFilter] = useState('all');
 
-    function handleFilterCategory (category) {
+    function handleFilterCategory(category) {
         setFilter(prevState => category);
     }
 
     return <div>
-        <div className = "btn-group btn-group-sm btn-block" style = {{marginBottom: '3px'}} role = "group">
-            <button type = "button"
-                    onClick = {() => handleFilterCategory('all')}
-                    className = "btn btn-secondary">All
+        <div className="btn-group btn-group-sm btn-block" style={{marginBottom: '3px'}} role="group">
+            <button type="button"
+                    onClick={() => handleFilterCategory('all')}
+                    className="btn btn-secondary">All
             </button>
-            <button type = "button"
-                    onClick = {() => handleFilterCategory('tv')}
-                    className = "btn btn-primary">TV
+            <button type="button"
+                    onClick={() => handleFilterCategory('tv')}
+                    className="btn btn-primary">TV
             </button>
-            <button type = "button"
-                    onClick = {() => handleFilterCategory('ftp')}
-                    className = "btn btn-info">FTP
+            <button type="button"
+                    onClick={() => handleFilterCategory('ftp')}
+                    className="btn btn-info">FTP
             </button>
-            <button type = "button"
-                    onClick = {() => handleFilterCategory('movie')}
-                    className = "btn btn-success">Movie
+            <button type="button"
+                    onClick={() => handleFilterCategory('movie')}
+                    className="btn btn-success">Movie
             </button>
         </div>
-        <table className = "table table-sm">
-            <caption style = {{
+        <table className="table table-sm">
+            <caption style={{
                 captionSide: "top",
                 textAlign: "center",
                 background: "#9dcca8",
@@ -35,28 +35,28 @@ export default function (props) {
                 marginBottom: "3px",
                 color: "#ffffff"
             }}>{`Total ${props.availableServers.length} available server(s)`}</caption>
-            <thead className = "thead-dark">
-            <tr>
-                <th>#</th>
-                <th>Category</th>
-                <th>Name</th>
-            </tr>
+            <thead className="thead-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Category</th>
+                    <th>Name</th>
+                </tr>
             </thead>
             <tbody>
-            {props.availableServers.filter(server => {
-                return filter === 'all' || server['category'].toLowerCase() === filter;
-            }).map((server, idx) => {
-                return <tr key = {server['category'] + '-' + server['host']}>
-                    <td>{idx + 1}</td>
-                    <td>{server['category']}</td>
-                    <td>
-                        <a target = '_blank'
-                           rel = "noopener noreferrer"
-                           className = "btn btn-block btn-info"
-                           href = {server['host']}>{server['name']}</a>
-                    </td>
-                </tr>
-            })}
+                {props.availableServers.filter(server => {
+                    return filter === 'all' || server['category'].toLowerCase() === filter;
+                }).map((server, idx) => {
+                    return <tr key={server['category'] + '-' + server['host']}>
+                        <td>{idx + 1}</td>
+                        <td>{server['category']}</td>
+                        <td>
+                            <a target='_blank'
+                               rel="noopener noreferrer"
+                               className="btn btn-block btn-info"
+                               href={server['host']}>{server['name']}</a>
+                        </td>
+                    </tr>
+                })}
             </tbody>
         </table>
     </div>
--- src/LeftSidebar.js
+++ src/LeftSidebar.js
@@ -1,24 +1,24 @@
 import React, {useRef, useState} from "react";
 
-export default function (props) {
+export default function LeftSidebar(props) {
 
     const filterListServerTxt = useRef();
     const [filteredServer, setFilteredServer] = useState([]);
 
-    function testClickHandler () {
+    function testClickHandler() {
         props.serverTestHandler();
     }
 
-    function disclaimerToggleHandler (e) {
+    function disclaimerToggleHandler(e) {
         props.disclaimerVisibilityToggler();
     }
 
-    function handleFilterListedServer (e) {
+    function handleFilterListedServer(e) {
         let token = filterListServerTxt.current.value.trim().toLowerCase();
         let matches = token.match(/(https?:\/\/)?(.*)/)
 
         token = matches && matches[2] ? matches[2] : '';
-        if ( !token.length ) {
+        if (!token.length) {
             setFilteredServer(prevState => []);
             return;
         }
@@ -30,62 +30,62 @@ export default function (props) {
         setFilteredServer(prevState => filteredServers.length ? filteredServers : []);
     }
 
-    return <div className = "col-md-4 col-xs-12" style = {{paddingBottom: '5px'}}>
+    return <div className="col-md-4 col-xs-12" style={{paddingBottom: '5px'}}>
         {/*Show Lifetime visitor count*/}
-        <div className = "alert alert-info" role = "alert">
+        <div className="alert alert-info" role="alert">
             Lifetime visitor: {props.lifetime}
         </div>
         {/*Show today's visitor count*/}
-        <div className = "alert alert-info" role = "alert">
+        <div className="alert alert-info" role="alert">
             Today's visitor: {props.today}
         </div>
         {/*Show total listed server count*/}
-        <div className = "alert alert-info" role = "alert">
+        <div className="alert alert-info" role="alert">
             Listed Servers: [<strong>{props.listedServer.length}</strong>]
         </div>
         {/*Show user's connection information*/}
         {Object.keys(props.connection.details).map(key => {
-            return <div key = {key} className = {`alert alert-${props.connection.alert}`} role = "alert">
+            return <div key={key} className={`alert alert-${props.connection.alert}`} role="alert">
                 {`[${key.toUpperCase()}]:`} <strong>{`${props.connection.details[key]}`}</strong>
             </div>
         })}
         {/*Test Button*/}
-        <div className = "form-group">
-            <div className = "btn-group btn-block" role = "group">
-                <button onClick = {testClickHandler} className = "btn btn-primary">Find Available Servers</button>
-                <button onClick = {disclaimerToggleHandler} className = "btn btn-warning">Disclaimer</button>
+        <div className="form-group">
+            <div className="btn-group btn-block" role="group">
+                <button onClick={testClickHandler} className="btn btn-primary">Find Available Servers</button>
+                <button onClick={disclaimerToggleHandler} className="btn btn-warning">Disclaimer</button>
             </div>
         </div>
-        <div className = "form-group">
-            <input type = "text" onChange = {handleFilterListedServer} ref = {filterListServerTxt}
-                   placeholder = "Search in listed servers" className = "form-control" />
-            {filteredServer.length ? <small className = "form-text text-info text-right">
+        <div className="form-group">
+            <input type="text" onChange={handleFilterListedServer} ref={filterListServerTxt}
+                   placeholder="Search in listed servers" className="form-control"/>
+            {filteredServer.length ? <small className="form-text text-info text-right">
                 Visit following websites by clicking the names
             </small> : null}
         </div>
         {filteredServer.length ? filteredServer.map((server, idx) => {
-            return <table className = "table table-bordered table-sm" key = {idx}>
+            return <table className="table table-bordered table-sm" key={idx}>
                 <tbody>
-                <tr>
-                    <td>
-                        <a href = {`${server['host']}`}
-                           className = {`btn btn-sm btn-block btn-${server['disabled'] ? 'danger' : 'success'}`}
-                           target = "_blank"
-                           rel = "noopener noreferrer">
-                            {`${server['name']} - [${server['category']}][${server['disabled'] ?
-                                'Disabled' :
-                                'Enabled'}]`}
-                        </a>
-                    </td>
-                    <td>
-                        <a className = "btn btn-sm btn-secondary btn-block"
-                           target = "_blank"
-                           rel = "noopener noreferrer"
-                           href = {`${server['special'] ?
-                               server['path'] :
-                               server['host'] + server['path']}`}>Resolver ({server['resolver'] ?? 'image'})</a>
-                    </td>
-                </tr>
+                    <tr>
+                        <td>
+                            <a href={`${server['host']}`}
+                               className={`btn btn-sm btn-block btn-${server['disabled'] ? 'danger' : 'success'}`}
+                               target="_blank"
+                               rel="noopener noreferrer">
+                                {`${server['name']} - [${server['category']}][${server['disabled'] ?
+                                    'Disabled' :
+                                    'Enabled'}]`}
+                            </a>
+                        </td>
+                        <td>
+                            <a className="btn btn-sm btn-secondary btn-block"
+                               target="_blank"
+                               rel="noopener noreferrer"
+                               href={`${server['special'] ?
+                                   server['path'] :
+                                   server['host'] + server['path']}`}>Resolver ({server['resolver'] ?? 'image'})</a>
+                        </td>
+                    </tr>
                 </tbody>
             </table>;
         }) : null}
